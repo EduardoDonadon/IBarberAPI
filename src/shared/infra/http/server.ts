04e3c150c3f2d59f3d8cbe93a3973c62f7485ca3
eduardodonadon <dona.edu2@gmail.com>
2021-11-26 15:01:45 -0300

    user avatar static route

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,9 +4,10 @@ import express, { NextFunction, Response, Request } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import uploadConfig from '@config/storage';
 import '@shared/infra/typeorm';
-
 import '@shared/providers';
+
 
 import { routes } from './routes';
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 
 app.use(routes);
 
